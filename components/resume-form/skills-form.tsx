a93a7c7142d4c436c579d2675e6cc848c931b1a5
dhanushtheijas08 <21cs033@kpriet.ac.in>
2024-06-19 14:51:26 +0530

    feat: Add SkillsForm component

---
+++ components/resume-form/skills-form.tsx
@@ -0,0 +1,174 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { ExperienceSchema } from "@/constants/schema";
+import { Experience } from "@/constants/types";
+
+import { Plus } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "../ui/textarea";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Model,
+  ModelTrigger,
+  ModelContent,
+  ModelHeader,
+  ModelBody,
+  ModelFooter,
+} from "@/components/responsive-model";
+
+const SkillsForm = () => {
+  const form = useForm<Experience>({
+    resolver: zodResolver(ExperienceSchema),
+  });
+
+  function onSubmit(data: Experience) {
+    console.log(data);
+  }
+
+  return (
+    <div className="max-w-[550px]">
+      <Model>
+        <ModelTrigger className="w-full">
+          <div className="flex justify-center items-center gap-2 border-[1.5px] border-dashed py-4 cursor-pointer">
+            <Plus className="w-5 h-5" />
+            <p>Add a new item</p>
+          </div>
+        </ModelTrigger>
+
+        <ModelContent>
+          <ModelHeader className="font-semibold text-xl">
+            Experience Section
+          </ModelHeader>
+          <ModelBody className="max-h-screen overflow-y-scroll scrollbar-none">
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="w-full md:px-1 mb-2 md:mb-1 grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-4"
+              >
+                <FormField
+                  control={form.control}
+                  name="experience.company"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Company Name</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          className="text-base"
+                          placeholder="Company Name"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="experience.role"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormLabel>Role</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          className="text-base"
+                          placeholder="Frontend Developer"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="experience.startDate"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormLabel>Start Date</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          className="text-base"
+                          placeholder=""
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="experience.endDate"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormLabel>End Date</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          placeholder=""
+                          className="text-base"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="experience.location"
+                  render={({ field }) => (
+                    <FormItem className="col-span-2">
+                      <FormLabel>Location</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          placeholder=""
+                          className="text-base"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="experience.description"
+                  render={({ field }) => (
+                    <FormItem className="col-span-2">
+                      <FormLabel>Description</FormLabel>
+                      <FormControl>
+                        <Textarea {...field} placeholder="" />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <ModelFooter className="p-0 col-span-2">
+                  <Button type="submit" className="w-full mt-2">
+                    Save changes
+                  </Button>
+                </ModelFooter>
+              </form>
+            </Form>
+          </ModelBody>
+        </ModelContent>
+      </Model>
+    </div>
+  );
+};
+
+export default SkillsForm;
