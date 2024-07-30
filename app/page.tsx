03f6f23b7b3f38ff192414cfbae8583395e0b66e
dhanushtheijas08 <21cs033@kpriet.ac.in>
2024-07-30 22:43:57 +0530

    style: Update hero section margin and footer styling

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ const Page = () => {
 
       <Header />
       <BlurText>
-        <div className="px-4 mt-10 flex flex-col items-center gap-6 w-full overflow-hidden mb-8">
+        <div className="px-4 mt-10 flex flex-col items-center gap-6 w-full overflow-hidden mb-8 md:mb-12 flex-1">
           <h1 className="text-center font-space-grotesk font-semibold  leading-none tracking-tighter bg-gradient-to-br bg-clip-text text-transparent from-white from-5% to-white/40 text-3xl sm:text-5xl md:text-6xl">
             Create Your Professional <br /> Resume in Minutes
           </h1>
@@ -50,7 +50,7 @@ const Page = () => {
         <KeyFeatures />
       </div> */}
 
-      <footer className="relative py-6 md:py-0">
+      <footer className="relative py-6 md:py-0 ">
         <div className="absolute top-0 h-px w-full footer" />
         <div className="container flex flex-col items-center justify-between gap-4 md:h-16 md:flex-row">
           <span className="text-xs sm:text-base font-space-grotesk text-muted-foreground/85">
